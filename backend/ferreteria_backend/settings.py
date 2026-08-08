b69fe745269f00c950d2488c4e9527699719a5ed
Herrera Cruz Vielka Ailyn <233112267@upmh.edu.mx>
2026-08-07 20:15:09 -0600

    Fix:Horario de mexico arreglado gmail

--- backend/ferreteria_backend/settings.py
+++ backend/ferreteria_backend/settings.py
@@ -97,8 +97,9 @@ DATABASES = {
 
 
 # Internationalization
+# Arreglar horario de Mexico
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 USE_TZ = True
 
